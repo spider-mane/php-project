502a0d53f414f12ed36ee5fa377b4b1eb5e9fc52
Chris Williams <spider.mane.web@gmail.com>
2019-10-28 18:56:56 -0400

    initial commit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+namespace :vendor_psr4\:package_psr4;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
