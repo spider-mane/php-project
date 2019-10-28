ccd9c11d342937550ab3fdcb13788ebec8dd9f09
Chris Williams <spider.mane.web@gmail.com>
2019-10-28 19:38:26 -0400

    added fields to configure-skeleton.sh

--- configure-skeleton.sh
+++ configure-skeleton.sh
@@ -3,26 +3,57 @@
 git_name=`git config user.name`;
 git_email=`git config user.email`;
 
+# author name
 read -p "Author name ($git_name): " author_name
 author_name=${author_name:-$git_name}
 
+# author email
 read -p "Author email ($git_email): " author_email
 author_email=${author_email:-$git_email}
 
+# author username
 username_guess=${author_name//[[:blank:]]/}
-read -p "Author username ($username_guess): " author_username
+read -p "Author github username ($username_guess): " author_username
 author_username=${author_username:-$username_guess}
 
+# author website
+author_website_guess="https://github.com/${author_username}"
+read -p "Author website ($author_website_guess): " author_website
+author_website=${author_website:-$author_website_guess}
+
+# vendor
+read -p "Vendor ($author_username): " vendor
+vendor=${vendor:-$author_username}
+
+# vendor_github
+read -p "Vendor github name ($vendor): " vendor_github
+vendor_github=${vendor_github:-$vendor}
+
+# package name
 current_directory=`pwd`
 current_directory=`basename $current_directory`
 read -p "Package name ($current_directory): " package_name
 package_name=${package_name:-$current_directory}
 
+# package homepage
+homepage_guess="https://github.com/{$vendor_github}/${package_name}"
+read -p "Package homepage ($homepage_guess): " homepage
+homepage=${homepage:-$homepage_guess}
+
+# package description
 read -p "Package description: " package_description
 
+# vendor psr-4
+read -p "Vendor psr-4: " vendor_psr4
+
+# package psr-4
+read -p "Package psr-4: " package_psr4
+
 echo
-echo -e "Author: $author_name ($author_username, $author_email)"
+echo -e "Author: $author_name ($author_username, $author_email, $author_website)"
+echo -e "Vendor: $vendor ($vendor_github)"
 echo -e "Package: $package_name <$package_description>"
+echo -e "psr-4 autoload $vendor_psr4\\\\$package_psr4\\\\"
 
 echo
 echo "This script will replace the above values in all files in the project directory and reset the git repository."
@@ -42,10 +73,16 @@ git init
 echo
 
 find . -type f -exec sed -i '' -e "s/:author_name/$author_name/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_username/$author_username/" {} \;
 find . -type f -exec sed -i '' -e "s/:author_email/$author_email/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_username/$author_username/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_website/$author_website/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor/$vendor/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor_github/$vendor_github/" {} \;
 find . -type f -exec sed -i '' -e "s/:package_name/$package_name/" {} \;
+find . -type f -exec sed -i '' -e "s/:homepage/$homepage/" {} \;
 find . -type f -exec sed -i '' -e "s/:package_description/$package_description/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor_psr4/$vendor_psr4/" {} \;
+find . -type f -exec sed -i '' -e "s/:package_psr4/$package_psr4/" {} \;
 
 sed -i '' -e "/^\*\*Note:\*\* Replace/d" README.md
 
