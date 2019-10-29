7bb7b3ace70b04169c55578ec98732d06d71f839
Chris Williams <spider.mane.web@gmail.com>
2019-10-28 20:30:59 -0400

    again

--- configure-skeleton.sh
+++ configure-skeleton.sh
@@ -17,7 +17,7 @@ read -p "Author github username ($username_guess): " author_username
 author_username=${author_username:-$username_guess}
 
 # author website
-author_website_guess=https://github.com/${author_username}
+author_website_guess=`https://github.com/${author_username}`
 read -p "Author website ($author_website_guess): " author_website
 author_website=${author_website:-$author_website_guess}
 
@@ -36,7 +36,7 @@ read -p "Package name ($current_directory): " package_name
 package_name=${package_name:-$current_directory}
 
 # package homepage
-homepage_guess=https://github.com/${vendor_github}/${package_name}
+homepage_guess=`https://github.com/${vendor_github}/${package_name}`
 read -p "Package homepage ($homepage_guess): " homepage
 homepage=${homepage:-$homepage_guess}
 
@@ -72,17 +72,17 @@ git init
 
 echo
 
-find . -type f -exec sed -i '' -e "s/:author_name/${author_name}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_email/${author_email}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_username/${author_username}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_website/${author_website}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor/${vendor}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor_github/${vendor_github}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_name/${package_name}/" {} \;
-find . -type f -exec sed -i '' -e "s/:homepage/${homepage}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_description/${package_description}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor_psr4/${vendor_psr4}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_psr4/${package_psr4}/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_name/{$author_name}/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_email/{$author_email}/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_username/{$author_username}/" {} \;
+find . -type f -exec sed -i '' -e "s/:author_website/{$author_website}/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor/{$vendor}/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor_github/{$vendor_github}/" {} \;
+find . -type f -exec sed -i '' -e "s/:package_name/{$package_name}/" {} \;
+find . -type f -exec sed -i '' -e "s/:homepage/{$homepage}/" {} \;
+find . -type f -exec sed -i '' -e "s/:package_description/{$package_description}/" {} \;
+find . -type f -exec sed -i '' -e "s/:vendor_psr4/{$vendor_psr4}/" {} \;
+find . -type f -exec sed -i '' -e "s/:package_psr4/{$package_psr4}/" {} \;
 
 sed -i '' -e "/^\*\*Note:\*\* Replace/d" README.md
 
