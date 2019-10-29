20819cb20bae2c4b164e14ff6230682631fb43cb
Chris Williams <spider.mane.web@gmail.com>
2019-10-28 22:00:20 -0400

    more things

--- prefill.php
+++ prefill.php
@@ -4,17 +4,19 @@ define('COL_HELP', 1);
 define('COL_DEFAULT', 2);
 
 $fields = [
-    'author_name' =>            ['Your name',             '',                                                ''],
-    'author_github_username' => ['Your Github username',  '<username> in https://github.com/username',       ''],
-    'author_email' =>           ['Your email address',    '',                                                ''],
-    'author_twitter' =>         ['Your twitter username', '',                                                '@{author_github_username}'],
-    'author_website' =>         ['Your website',          '',                                                'https://github.com/{author_github_username}'],
+    'author_name' => ['Your name', '', ''],
+    'author_github_username' => ['Your Github username', '<username> in https://github.com/username', ''],
+    'author_email' => ['Your email address', '', ''],
+    'author_website' => ['Your website', '', 'https://github.com/{author_github_username}'],
 
-    'package_vendor' =>         ['Package vendor',        '<vendor> in https://github.com/vendor/package',   '{author_github_username}'],
-    'package_name' =>           ['Package name',          '<package> in https://github.com/vendor/package',  ''],
-    'package_description' =>    ['Package very short description',   '',                                     ''],
+    'vendor_name' => ['Package vendor', '', '{author_github_username}'],
+    'vendor_github' => ['Vendor github name', '', '{vendor_name}'],
 
-    'psr4_namespace' =>         ['PSR-4 namespace',       'usually, Vendor\\Package',                        '{package_vendor}\\{package_name}'],
+    'package_name' => ['Package name', '', ''],
+    'package_homepage' => ['Package homepage', '', 'https://github.com/{vendor_github}/{package_name}'],
+    'package_description' => ['Package very short description', '', ''],
+
+    'psr4_namespace' => ['PSR-4 namespace', 'usually, Vendor\\Package', ''],
 ];
 
 $values = [];
@@ -23,28 +25,28 @@ $replacements = [
     ':vendor_name\\\\:package_name\\\\' => function () use (&$values) {
         return str_replace('\\', '\\\\', $values['psr4_namespace']) . '\\\\';
     },
-    ':author_name'                 => function () use (&$values) {
+    ':author_name' => function () use (&$values) {
         return $values['author_name'];
     },
-    ':author_username'             => function () use (&$values) {
+    ':author_username' => function () use (&$values) {
         return $values['author_github_username'];
     },
-    ':author_website'              => function () use (&$values) {
+    ':author_website' => function () use (&$values) {
         return $values['author_website'] ?: ('https://github.com/' . $values['author_github_username']);
     },
-    ':author_email'                => function () use (&$values) {
+    ':author_email' => function () use (&$values) {
         return $values['author_email'] ?: ($values['author_github_username'] . '@example.com');
     },
-    ':vendor_name'                      => function () use (&$values) {
-        return $values['package_vendor'];
+    ':vendor_name' => function () use (&$values) {
+        return $values['vendor_name'];
     },
-    ':package_name'                => function () use (&$values) {
+    ':package_name' => function () use (&$values) {
         return $values['package_name'];
     },
-    ':package_description'         => function () use (&$values) {
+    ':package_description' => function () use (&$values) {
         return $values['package_description'];
     },
-    'League\\Skeleton'             => function () use (&$values) {
+    'League\\Skeleton' => function () use (&$values) {
         return $values['psr4_namespace'];
     },
 ];
--- prefill.sh
+++ prefill.sh
@@ -17,8 +17,7 @@ read -p "Author github username ($username_guess): " author_username
 author_username=${author_username:-$username_guess}
 
 # author website
-author_website_guess="https://github.com"
-# author_website_guess="https://github.com/${author_username}"
+author_website_guess="https://github.com/${author_username}"
 read -p "Author website ($author_website_guess): " author_website
 author_website=${author_website:-$author_website_guess}
 
@@ -36,10 +35,10 @@ current_directory=`basename $current_directory`
 read -p "Package name ($current_directory): " package_name
 package_name=${package_name:-$current_directory}
 
-# package homepage
-homepage_guess="https://github.com/${vendor_github}/${package_name}"
-read -p "Package homepage ($homepage_guess): " homepage
-homepage=${homepage:-$homepage_guess}
+# package website
+package_website_guess="https://github.com/${vendor_github}/${package_name}"
+read -p "Package package_website ($package_website_guess): " package_website
+package_website=${package_website:-$package_website_guess}
 
 # package description
 read -p "Package description: " package_description
@@ -53,7 +52,7 @@ read -p "Package psr-4: " package_psr4
 echo
 echo -e "Author: $author_name ($author_username, $author_email, $author_website)"
 echo -e "Vendor: $vendor ($vendor_github)"
-echo -e "Package: $package_name ($homepage) <$package_description>"
+echo -e "Package: $package_name ($package_website) <$package_description>"
 echo -e "psr-4 autoload: $vendor_psr4\\$package_psr4\\"
 
 echo
@@ -73,19 +72,19 @@ git init
 
 echo
 
-find . -type f -exec sed -i '' -e "s/:author_name/${author_name}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_email/${author_email}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_username/${author_username}/" {} \;
-find . -type f -exec sed -i '' -e "s/:author_website/${author_website}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor_name/${vendor_name}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor_name_github/${vendor_github}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_name/${package_name}/" {} \;
-find . -type f -exec sed -i '' -e "s/:homepage/${homepage}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_description/${package_description}/" {} \;
-find . -type f -exec sed -i '' -e "s/:vendor_psr4/${vendor_psr4}/" {} \;
-find . -type f -exec sed -i '' -e "s/:package_psr4/${package_psr4}/" {} \;
+find -type f -exec sed -i -e "s/:author_name/${author_name}/"
+find -type f -exec sed -i -e "s/:author_email/${author_email}/"
+find -type f -exec sed -i -e "s/:author_username/${author_username}/"
+find -type f -exec sed -i -e "s/:author_website/${author_website}/"
+find -type f -exec sed -i -e "s/:vendor_name/${vendor_name}/"
+find -type f -exec sed -i -e "s/:vendor_name_github/${vendor_github}/"
+find -type f -exec sed -i -e "s/:package_name/${package_name}/"
+find -type f -exec sed -i -e "s/:package_website/${package_website}/"
+find -type f -exec sed -i -e "s/:package_description/${package_description}/"
+find -type f -exec sed -i -e "s/:vendor_psr4/${vendor_psr4}/"
+find -type f -exec sed -i -e "s/:package_psr4/${package_psr4}/"
 
-sed -i '' -e "/^\*\*Note:\*\* Replace/d" README.md
+sed -i -e "/^\*\*Note:\*\* Replace/d" README.md
 
 echo "Replaced all values and reset git directory, self destructing in 3... 2... 1..."
 
